6a2065067abd0e8c2f549f87ffa52c66e7d0bbfd
seenivasan <seeni0424@gmail.com>
2018-01-09 18:11:44 +0530

    Register Module Working

--- Register.php
+++ Register.php
@@ -4,28 +4,10 @@
 	<title>Register New Account</title>
 </head>
 <body>
-<?php 
-	$UserName=$_POST["UserName"];
-	$Password=$_POST["Password"];
-	try{
-		$conn=new PDO("mysql:host=localhost;dbname=CarPool","guestPooler","qwerty1234");
-		$sta=$conn->prepare("Select * from Users;");
-		$sta->execute();
-		while ($row = $sta->fetch()) {
-			print($row["UserName"] . "\t" . $row["password"]);
-		}
-		echo "\nDone";
-	}
-	catch(Exception $e){
-		print($e);
-	}
-
-?>
-<form method="POST" action="Register.php">
-	<p>UserName<input type="text" name="UserName"></p>
-	<p>Password<input type="password" value=".................." name="Password"></p>
+<form method="POST" action="Register Status.php">
+	<p>UserName<input type="text" name="UserName" required=""></p>
+	<p>Password<input type="password" name="Password" required=""></p>
 	<p><input type="Submit" name="Submit" value="Create"></p>
 </form>
-
 </body>
 </html>
